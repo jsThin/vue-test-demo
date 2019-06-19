ef4379ac88dcfd73d90b519e2e502da7a1f6e74c
limuqing123 <1397484550@qq.com>
2019-06-19 17:03:02 +0800

    上传基础项目

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello-world></hello-world>
+    <rx-demo></rx-demo>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/hello-world/hello-world';
+import RxDemo from './components/rx-demo/rx-demo';
+
+export default {
+  name: 'App',
+  provide() {
+    return {
+      GLOBAL: {
+        logined: true,
+      },
+    };
+  },
+  components: {
+    HelloWorld,
+    RxDemo,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
